3b821e0b10a7bbb80b2004515046fd00ed735da8
ZAIN-mashhood <zainmashhood9966@gmail.com>
2024-06-24 20:51:22 +0500

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/A_Complete_Responsive_Website_Using_Bootstrap_And_ReactJS/"
 })
