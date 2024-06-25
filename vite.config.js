f8809e0677a5b68b3e758cd11bdeac787052a799
ZAIN-mashhood <zainmashhood9966@gmail.com>
2024-06-25 23:10:25 +0500

    deploy react to github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/A_Complete_Responsive_Website_Using_Bootstrap_And_ReactJS/"
+  base: "/A_Complete_Responsive_Website_Using_Bootstrap_And_ReactJS/",
+  plugins: [react()],  
 })
